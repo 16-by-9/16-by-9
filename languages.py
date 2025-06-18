efea0e7fdc1a43c768209794738e885d8f6475c3
16-by-9 <abupdown8@gmail.com>
2025-06-18 15:44:16 +0300

    Update languages.py
    Add JSON support

--- languages.py
+++ languages.py
@@ -14,6 +14,7 @@ LANGUAGES_EXT = {
     "C#": [".cs"],
     "Java": [".java", ".jar"],
     "JavaScript": [".js"],
+    "JSON": [".json"],
     "TypeScript": [".ts"],
     "Go": [".go"],
     "Rust": [".rs"],
